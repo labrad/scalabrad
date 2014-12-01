5900abc0ddded8e3150befa674e7149bdf68e9e0
Matthew Neeley <mneeley@nestlabs.com>
2014-12-01 07:24:28 -0800

    Remove in-file copyright notices.

--- src/main/java/org/labrad/annotations/Setting.java
+++ src/main/java/org/labrad/annotations/Setting.java
@@ -1,22 +1,3 @@
-/*
- * Copyright 2008 Matthew Neeley
- *
- * This file is part of JLabrad.
- *
- * JLabrad is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * JLabrad is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with JLabrad.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package org.labrad.annotations;
 
 import java.lang.annotation.ElementType;
